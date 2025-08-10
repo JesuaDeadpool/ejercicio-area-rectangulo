c32a597f1fd1ad48aed269c80cc8ef2586fdb737
Jesua <144072667+JesuaDeadpool@users.noreply.github.com>
2025-08-09 20:12:51 -0600

    codigo inicial del ejercicio area rectangulo

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+const calcular = () => {
+    const base = document.querySelector('#base').value;
+    const altura = document.querySelector('#altura').value;
+    if (base === "") {
+        alerta("El campo 'Base' está vacio porfavor ingrese un numero ");
+    } else if (altura === "") {
+        alerta("El campo 'Altura' está vacio porfavor ingrese un numero ");
+    } else {
+        let baseNum = Number(base);
+        let alturaNum = Number(altura);
+
+        if (baseNum < 0) {
+            alerta("El campo 'Base', le ingreso un numero negativo, ingrese numero positivo");
+        } else if (alturaNum < 0) {
+            alerta("El campo 'Altura', le ingreso un numero negativo, ingrese numero positivo");
+        } else {
+            let calcArea = baseNum * alturaNum;
+
+            document.querySelector('#area').value = calcArea;
+        }
+    }
+
+
+}
+
+const limpiar = () => {
+    const base = document.querySelector('#base').value = "";
+    const altura = document.querySelector('#altura').value = "";
+    document.querySelector('#area').value = "";
+}
+
+const alerta = (mensaje) => {
+    Swal.fire({
+        title: mensaje,
+        icon: "warning"
+    })
+}
